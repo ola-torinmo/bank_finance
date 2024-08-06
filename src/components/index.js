93a848d893971f9183c9bb1fe3ad59887d5ce618
torinmo <torinmo.work@gmailcom>
2024-08-06 15:24:57 +0100

    full bank_finance landing page

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Navbar from "./Navbar"
+import CTA from "./CTA"
+import Clients from "./Clients"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Button from "./Button"
+import Billing from "./Billing"
+import Business from "./Business"
+import CardDeal from "./CardDeal"
+import FeedbackCard from "./FeedbackCard"
+import GetStarted from "./GetStarted"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+
+
+export{
+    Navbar,
+    CTA,
+    Clients,
+    Footer,
+    Hero,
+    Button,
+    Billing,
+    Business,
+    CardDeal,
+    FeedbackCard,
+    GetStarted,
+    Stats,
+    Testimonials,
+};
